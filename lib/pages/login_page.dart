743c2434a1f841be10f7905baf44534054115471
Idriss-rayan <rayanidriss27@gmail.com>
2025-02-10 16:47:39 +0600

    add

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -52,7 +52,7 @@ class _LoginPageState extends State<LoginPage> {
           //password
           TextField(
             controller: _passwordController,
-            decoration: const InputDecoration(labelText: "mot de pass"),
+            decoration: const InputDecoration(labelText: "mot de passs"),
             obscureText: true,
           ),
 
